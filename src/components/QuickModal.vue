fdc1cd50975461f10dcc90e44042f09597b6fbc5
Tim Dohmen <dohmentim@yahoo.com>
2020-07-30 17:53:28 -0600

    init

---
+++ src/components/QuickModal.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="quickModal" :class="dataToPass">
+    <!-- Modal -->
+    <div
+      class="modal fade"
+      :id="id"
+      tabindex="-1"
+      role="dialog"
+      aria-labelledby="modelTitleId"
+      aria-hidden="true"
+    >
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <slot name="title">Title</slot>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            <slot></slot>
+            <slot name="body"></slot>
+            <h3>{{modalProp}}</h3>
+          </div>
+          <div class="modal-footer">
+            <slot name="footer">Footzone</slot>
+
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            <button type="button" class="btn btn-primary">Save</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: "quickModal",
+  data() {
+    return {};
+  },
+  computed: {},
+  methods: {},
+  components: {},
+  props: ["id", "modalProp", "dataToPass"],
+};
+</script>
+
+
+<style scoped>
+</style>
---
+++ src/pages/Home.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="home">
+    <div class="row justify-content-center">
+      <h3>Modal</h3>
+      <img alt="Vue logo" src="../assets/logo.png" class="logo" />
+      <h3>iew</h3>
+    </div>
+    <div class="row buttons justify-content-center">
+      <button
+        type="button"
+        class="btn btn-primary btn-lg"
+        data-toggle="modal"
+        data-target="#one"
+      >Modal 1</button>
+      <button
+        type="button"
+        class="btn btn-secondary btn-lg"
+        data-toggle="modal"
+        data-target="#two"
+      >Modal 2</button>
+      <button type="button" class="btn btn-danger btn-lg" @click="toggledOn = !toggledOn">Show Div</button>
+    </div>
+    <QuickModal id="one" :key="1" :modalProp="'this is my first modal'">
+      <h3>This is my body</h3>
+      <div slot="footer">This is my footer</div>
+      <div slot="body">This is fake body slot</div>
+      <div slot="title">This is my Title</div>
+    </QuickModal>
+    <QuickModal
+      id="two"
+      :dataToPass="tacos"
+      :modalProp="'this is my second modal'"
+    >This is different info</QuickModal>
+    <div class="form-check" v-if="toggledOn">
+      <label class="form-check-label">
+        <input type="checkbox" class="form-check-input" name id value="checkedValue" checked />
+        Display value
+      </label>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import QuickModal from "../components/QuickModal";
+export default {
+  name: "Home",
+  data() {
+    return {
+      toggledOn: false,
+    };
+  },
+  computed: {
+    tacos() {
+      return "tims tacos";
+    },
+  },
+  components: { QuickModal },
+};
+</script>
+
+<style scoped>
+.logo {
+  height: 30px;
+  width: 30px;
+}
+</style>
